7285f3dd976aadc1ca128e647cbfc64a600d9263
austinpentester <austinpentester@gmail.com>
2026-08-01 11:27:33 +0000

    berror issue fix

--- src/modules/product/dto/product-sub.dto.ts
+++ src/modules/product/dto/product-sub.dto.ts
@@ -6,6 +6,7 @@ import {
   IsOptional,
   IsString,
   IsUUID,
+  Matches,
   Min,
 } from 'class-validator';
 
@@ -60,7 +61,9 @@ export class UpdateInventoryDto {
 export class AssignCategoriesDto {
   @ApiProperty({ example: ['123e4567-e89b-12d3-a456-426614174001'] })
   @IsArray()
-  @IsUUID('4', { each: true })
+  @Matches(/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i, {
+    each: true,
+  })
   categoryIds: string[];
 }
 
